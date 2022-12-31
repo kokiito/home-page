c4f84c4dcc1bf091fd3e1388a183a4cc620f6d78
kokiito <itou-kouki@aidemy.co.jp>
2022-12-31 16:10:29 +0900

    feat: add biography section

---
+++ app/components/Sections/Biography.tsx
@@ -0,0 +1,13 @@
+import { Box, Heading, List, ListItem, Text, Link, useColorModeValue } from '@chakra-ui/react'
+
+export const Biography = () => {
+  const color = useColorModeValue('indigo', '#f5eb78')
+  return <Box p={['32px', '32px', '48px']}>
+    <Heading as='h1'>Biography</Heading>
+    <List mt={'24px'}>
+      <ListItem> <Text as='b'>1995</Text><Text>Born in Osaka, Japan</Text></ListItem>
+      <ListItem><Text as='b'>2020</Text><Text>Completed the Master's Program in the Graduate School of Information Science at Ritsumeikan university</Text></ListItem>
+      <ListItem><Text as='b'>2020</Text><Text>Started working at <Link isExternal color={color} href='https://aidemy.co.jp/'>Aidemy</Link></Text></ListItem>
+    </List>
+  </Box >
+}
